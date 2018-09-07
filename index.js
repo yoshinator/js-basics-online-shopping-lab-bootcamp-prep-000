989d68b65847225b893a7d46fbb0ef5350a0d80c
yoan ante <yoanante@gmail.com>
2018-09-07 15:48:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,28 @@ function addToCart(item) {
  return `${item} has been added to your cart.`
 }
 
+// function viewCart() {
+//   var statement = "";
+//   if (cart.length === 0){
+//     return "Your shopping cart is empty." 
+//   }
+//   else if (cart.length === 1){
+//     statement = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
+//     return statement+"."
+//   }
+//   //   else if (cart.length === 2){
+//   //   statement = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}`
+//   //   return statement+"."
+//   // }
+  
+//   else {
+//       statement = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}`
+//       for (var i = 2; i < cart.length - 1; i++){
+//       statement = statement+ `, ${cart[i].itemName} at $${cart[i].itemPrice}`
+//     }
+//   }return statement+`and ${cart[cart.length].itemName} at ${cart[cart.length].itemPrice}`;
+// }
+
 function viewCart() {
   var statement = "";
   if (cart.length === 0){
