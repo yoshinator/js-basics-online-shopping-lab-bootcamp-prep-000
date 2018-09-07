69d711a650828e1d389529ae420c7954864f94d5
yoan ante <yoanante@gmail.com>
2018-09-07 16:52:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
     if (cart[i].itemName === item){
-     return cart.splice(cart[i],1)
+     return cart.splice(i,1)
     }
   }return "That item is not in your cart."
 }
