0a3c338925da529dff4807dc62f6e9f04a75725b
yoan ante <yoanante@gmail.com>
2018-09-07 15:01:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function viewCart() {
   return "Your shopping cart is empty." 
   }else{
     for (var i = 0; i < cart.length; i++){
-      
+      statement = statement+ `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}`
     }
-  }
+  }return statement;
 }
 
 function total() {
