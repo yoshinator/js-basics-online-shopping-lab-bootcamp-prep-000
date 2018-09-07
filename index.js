e228472d3acf16eac5635901c86c9684d1bf1380
yoan ante <yoanante@gmail.com>
2018-09-07 16:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === null){
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
 
 
